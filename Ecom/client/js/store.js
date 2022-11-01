b106a17feacac31c29b289ea17ac01df6e30994e
binary-action55 <anirudhsundaram201@gmail.com>
2022-11-01 19:09:37 +0530

    cart add item dom

--- Ecom/client/js/store.js
+++ Ecom/client/js/store.js
@@ -1,6 +1,8 @@
 const cartModal = document.querySelector('.cartModal');
 const cartBtn = document.querySelector('.cartButton');
 const closeCart = document.querySelector('.closeCart');
+const productContent = document.querySelector('.productContent');
+const cartItemList = document.querySelector('.cartItemList');
 
 cartBtn.addEventListener('click',()=>{
     cartModal.classList.remove('hide');
@@ -8,5 +10,27 @@ cartBtn.addEventListener('click',()=>{
 
 closeCart.addEventListener('click',()=>{
     cartModal.classList.add('hide');
+})
+
+productContent.addEventListener('click',(e)=>{
+    const parent = e.target.parentElement;
+    const product = parent.dataset;
+    console.log(product);
+    const cartItem = document.createElement('div');
+    cartItem.classList.add('cartRow');
+    cartItem.innerHTML = `
+    <span class="cartColumn">
+        <img class="cartItemImage"
+            src="${product.imgurl}" />
+        <span class="cartItemName">${product.name}</span>
+    </span>
+    <span class="cartColumn">
+        <span class="cartItemPrice">${product.price}</span>
+    </span>
+    <span class="cartColumn">
+        <span class="cartItemQty">1</span>
+    </span>
+    `;
+    cartItemList.appendChild(cartItem);
 })
 
